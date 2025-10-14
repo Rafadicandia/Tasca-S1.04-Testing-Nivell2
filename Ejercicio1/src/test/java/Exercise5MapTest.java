8acd39a6388047a0348e12359384d320081e0087
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-14 13:30:22 +0200

    feat: building hashmap

---
+++ Ejercicio1/src/test/java/Exercise5MapTest.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Exercise5MapTest {
+
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+    // Add keys and values (Country, City)
+    capitalCities.put("England", "London");
+    capitalCities.put("India", "New Dehli");
+    capitalCities.put("Austria", "Wien");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("Norway", "Oslo"); // Duplicate
+    capitalCities.put("USA", "Washington DC");
+
+
+}
+
