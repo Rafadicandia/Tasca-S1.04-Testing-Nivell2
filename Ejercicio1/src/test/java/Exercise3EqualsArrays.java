03f8afeb01ebf1ef1d36bb7b76cb7c70b2901ab8
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-14 12:23:33 +0200

    refactor: adding two arrays for testing and writing de assert function

--- Ejercicio1/src/test/java/Exercise3EqualsArrays.java
+++ Ejercicio1/src/test/java/Exercise3EqualsArrays.java
@@ -5,6 +5,11 @@ public class Exercise3EqualsArrays {
 
     @Test
     public void equalArrays(){
-        
+
+        String [] fruits = {"Bannana", "Apple", "Peach" };
+        String [] fruits2 = {"Bannana", "Apple", "Peach" };
+
+        assertThat(fruits).containsExactly(fruits2);
+
     }
 }
