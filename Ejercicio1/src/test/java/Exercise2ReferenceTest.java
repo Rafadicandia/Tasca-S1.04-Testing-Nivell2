107179712f466e489ed050a7a6081e3274371694
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-14 12:13:24 +0200

    feat: adding not equal reference test

--- Ejercicio1/src/test/java/Exercise2ReferenceTest.java
+++ Ejercicio1/src/test/java/Exercise2ReferenceTest.java
@@ -1,11 +1,21 @@
 import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class Exercise2ReferenceTest {
 
     @Test
     public void equalReference(){
         String a = "Option a";
-        
+        String b = a;
+        assertThat(a).isSameAs(b);
+
+    }
+
+    @Test
+    public void notEqualReference(){
+        String a = "Option a";
+        String b = new String("Option a");
+        assertThat(a).isNotSameAs(b);
 
     }
 }
